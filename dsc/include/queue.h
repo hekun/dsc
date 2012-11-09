36c5aebe3afa159fc45edd31d61769012162d92e
He kun <hek1982@gmail.com>
2012-11-10 06:36:30 +0800

    链队列操作接口函数设计完成

---
+++ dsc/include/queue.h
@@ -0,0 +1,50 @@
+#ifndef QUEUE_INCLUDE
+#define QUEUE_INCLUDE
+
+#include "g_type.h"
+#include "v_data.h"
+
+#define QUEUE_T  queue_attr_t
+typedef struct  QUEUE_T *QUEUE_T;
+
+typedef enum queue_type_E
+{
+    UNKNOWN_STACK,
+    QUEUE_SIGNAL_LINK_LIST,
+    QUEUE_DOUBLE_LINK_LIST,
+    QUEUE_CIRCLE_LINK_LIST
+}queue_type_t;
+
+
+typedef Status  (*queue_visit) (void *val);
+typedef Status  (*InitQueue) (QUEUE_T * q);//构造一个空链队列
+typedef Status  (*DestroyQueue) (QUEUE_T *q);//销毁已建立的链队列
+typedef Status  (*ClearQueue) (QUEUE_T q);//将链队列中元素全部清空
+typedef Status  (*QueueEmpty) (QUEUE_T q);//判断链队列是否为空。
+typedef Int32_t (*QueueLength)(QUEUE_T q);//获取链队列元素总数
+typedef Status  (*GetHead)  (QUEUE_T q, v_type_t type, void **val, size_t size);//获取链队列头元素数值
+typedef Status  (*EnQueue)  (QUEUE_T q, v_type_t type, void *val, size_t size);//在链队列中追加尾元素
+typedef Status  (*DeQueue)  (QUEUE_T q, v_type_t type, void *val, size_t size);//删除链队列头元素
+typedef Status  (*QueueTraverse) (QUEUE_T q, queue_visit visit);//对链队列的每个元素执行visit函数。
+
+typedef struct queue_opt_funcs_S
+{
+    queue_visit visit;
+}queue_opt_funcs_t;
+
+typedef struct Queue_funcs_S
+{
+    InitQueue       init_queue;
+    DestroyQueue    destroy_queue;
+    ClearQueue      clear_queue;
+    QueueEmpty      queue_empty;
+    QueueLength     queue_length;
+    GetHead         get_head;
+    EnQueue         en_queue;
+    DeQueue         de_queue;
+    queue_visit     queue_visit;
+}Queue_funcs_t;
+
+#undef QUEUE_T
+#endif
+
